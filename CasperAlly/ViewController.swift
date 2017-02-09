68a623d8620e29823b6db960932bd6fd7ab8e80a
rsaeks <rsaeks@gmail.com>
2017-02-09 10:52:56 -0600

    Initial Commit

---
+++ CasperAlly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CasperAlly
+//
+//  Created by Randy Saeks on 2/9/17.
+//  Copyright © 2017 Randy Saeks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
