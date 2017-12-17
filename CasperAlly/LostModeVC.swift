491d001d185d0925aee6b99fca492e1bde37045f
Unknown <rsaeks@gmail.com>
2017-12-17 11:57:20 -0600

    Updated UI of LostMode Settings

--- CasperAlly/LostModeVC.swift
+++ CasperAlly/LostModeVC.swift
@@ -18,6 +18,8 @@ class LostModeVC: UIViewController {
     @IBOutlet weak var lostModeFootnoteText: UITextView!
     @IBOutlet weak var alwaysEnforceLostSwitch: UISwitch!
     @IBOutlet weak var lostModeSoundSwitch: UISwitch!
+    @IBOutlet weak var enableLostModeButton: actionButton!
+    @IBOutlet weak var closeButton: actionButton!
     
     var lmMessage: String = ""
     var lmPhone: String = ""
@@ -27,13 +29,12 @@ class LostModeVC: UIViewController {
     
     override func viewDidLoad() {
         
-
-        lostModeMessageText.layer.borderWidth = 1
-        lostModeMessageText.layer.borderColor = UIColor.lightGray.cgColor
-        lostModeFootnoteText.layer.borderWidth = 1
-        lostModeFootnoteText.layer.borderColor = UIColor.lightGray.cgColor
-        lostModePhoneNumberText.layer.borderWidth = 1
-        lostModePhoneNumberText.layer.borderColor = UIColor.lightGray.cgColor
+        enableLostModeButton.layer.borderWidth = 1.0
+        enableLostModeButton.layer.borderColor = UIColor.lightGray.cgColor
+        closeButton.layer.borderWidth = 1.0
+        closeButton.layer.borderColor = UIColor.lightGray.cgColor
+        
+        setButtonColors(color: UIColor.lightGray.cgColor, width: 1.0)
         
         print("View did load....")
         super.viewDidLoad()
@@ -46,6 +47,7 @@ class LostModeVC: UIViewController {
         else {
             print("No Saved Lost Mode Message Present...initilaizing")
             lmMessage = "Default Lost Mode Message"
+            lostModeMessageText.text = lmMessage
             LostMode.lostModeSettings.lostModeMessage = lmMessage
         }
         
@@ -57,6 +59,7 @@ class LostModeVC: UIViewController {
         else {
             print("No Saved Lost Mode Phone Present...initilaizing")
             lmPhone = "123-456-7890"
+            lostModePhoneNumberText.text = lmPhone
             LostMode.lostModeSettings.lostModeNumber = lmPhone
         }
         
@@ -68,6 +71,7 @@ class LostModeVC: UIViewController {
         else {
             print("No Saved Lost Mode Footnote Present...initilaizing")
             lmFootnote = "Device is lost"
+            lostModeFootnoteText.text = lmFootnote
             LostMode.lostModeSettings.lostModeFootNote = lmFootnote
         }
         
@@ -94,107 +98,86 @@ class LostModeVC: UIViewController {
             lostModeSoundSwitch.isOn = true
             LostMode.lostModeSettings.lostModeSound = true
         }
-        
-        // Let's setup our view data
-        //lostModeMessageText.text = lmMessage
-        //lostModePhoneNumberText.text = lmPhone
-        //lostModeFootnoteText.text = lmFootnote
-//        alwaysEnforceLostSwitch.isOn = lmAlwaysEnforce
-//        lostModeSoundSwitch.isOn = lmSound
-        
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-
-        cameFromLostMode = true
-        print("Setting came from lost mode to true:\(cameFromLostMode)")
-//        print("View appeared ... here is the loaded data")
-//        print("Message to use: \(lmMessage)")
-//        print("Has length: \(lmMessage.count)")
-//        print("Phone number to use: \(lmPhone)")
-//        print("Footnote to use: \(lmFootnote)")
-//        print("Enforced Lost Mode status: \(lmAlwaysEnforce)")
-//        print("Play sound Status: \(lmSound)")
-//        print("Now to pass back data....")
-//        
-//        print("Here are settings as of View Did Appear:")
-//        print("Message: \(String(describing: lostModeMessageText.text))")
-//        //print("Message length for lost message: \(String(describing: lostModeMessageText.text?.count))")
-//        print("Phone: \(String(describing: lostModePhoneNumberText.text))")
-//        print("Message length for phone : \(String(describing: lostModePhoneNumberText.text?.count))")
-//        print("Footnote: \(String(describing: lostModeFootnoteText.text))")
-//        print("Message length for footnote: \(String(describing: lostModeFootnoteText.text?.count))")
-//        print("Always Enforce: \(alwaysEnforceLostSwitch.isOn)")
-//        print("Play sound: \(lostModeSoundSwitch.isOn)")
-        
-       
-        
-    }
-    
-    
-    @IBAction func closeButtonPressed(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
-    }
-    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) { self.view.endEditing(true) }
+    override func viewDidAppear(_ animated: Bool) { cameFromLostMode = true }
+    @IBAction func closeButtonPressed(_ sender: Any) { dismiss(animated: true, completion: nil) }
     
     
     @IBAction func enableLostModePressed(_ sender: UIButton) {
         
-    
-        defaultsLM.set(lostModeMessageText.text, forKey: "savedLostModeMessage")
+        setButtonColors(color: UIColor.lightGray.cgColor, width: 1)
+        
         defaultsLM.set(lostModePhoneNumberText.text, forKey: "savedLostModePhone")
         defaultsLM.set(lostModeFootnoteText.text, forKey: "savedLostModeFootnote")
         defaultsLM.set(alwaysEnforceLostSwitch.isOn, forKey: "savedLostModeForced")
         defaultsLM.set(lostModeSoundSwitch.isOn, forKey: "savedLostModeSound")
         
-        LostMode.lostModeSettings.lostModeMessage = lostModeMessageText.text
-        LostMode.lostModeSettings.lostModeNumber = lostModePhoneNumberText.text
-        LostMode.lostModeSettings.lostModeFootNote = lostModeFootnoteText.text
+//        LostMode.lostModeSettings.lostModeMessage = lostModeMessageText.text
+//        LostMode.lostModeSettings.lostModeNumber = lostModePhoneNumberText.text
+//        LostMode.lostModeSettings.lostModeFootNote = lostModeFootnoteText.text
         LostMode.lostModeSettings.lostModeForced = alwaysEnforceLostSwitch.isOn
         LostMode.lostModeSettings.lostModeSound = lostModeSoundSwitch.isOn
+    
         
-        sender.layer.borderWidth = 2.0
-        sender.layer.borderColor = warnColor.cgColor
+        print("Lost Mode Message contains text test: \(lostModeMessageText.hasText)")
+        print("Lost Mode Phone Number contains text test: \(lostModePhoneNumberText.hasText)")
+        print("Lost Mode Footnteo contains text: \(lostModeFootnoteText.hasText)")
         
-        let headers = [ "Content-Type":"application/xml", ]
-        
-        // Custom Body Encoding
-        struct RawDataEncoding: ParameterEncoding {
-            public static var `default`: RawDataEncoding { return RawDataEncoding() }
-            public func encode(_ urlRequest: URLRequestConvertible, with parameters: Parameters?) throws -> URLRequest {
-                var request = try urlRequest.asURLRequest()
-                //                print("Incoming Data....")
-                //                print(LostMode.lostModeSettings.lostModeMessage) - Done
-                //                print(LostMode.lostModeSettings.lostModeNumber) - Done
-                //                print(LostMode.lostModeSettings.lostModeFootNote) - Done
-                //                print(LostMode.lostModeSettings.lostModeForced) - Done
-                //                print(LostMode.lostModeSettings.lostModeSound)
-                request.httpBody = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><mobile_device_command><general><command>EnableLostMode</command><lost_mode_message>\(LostMode.lostModeSettings.lostModeMessage!)</lost_mode_message><lost_mode_phone>\(LostMode.lostModeSettings.lostModeNumber!)</lost_mode_phone><lost_mode_footnote>\(LostMode.lostModeSettings.lostModeFootNote!)</lost_mode_footnote><always_enforce_lost_mode>\(LostMode.lostModeSettings.lostModeForced!)</always_enforce_lost_mode><lost_mode_with_sound>\(LostMode.lostModeSettings.lostModeSound!)</lost_mode_with_sound></general><mobile_devices><mobile_device><id>\(workingData.deviceID)</id></mobile_device></mobile_devices></mobile_device_command>".data(using: String.Encoding.utf8, allowLossyConversion: false)
-                return request
+        if (lostModeMessageText.hasText && lostModePhoneNumberText.hasText && lostModeFootnoteText.hasText) {
+            sender.layer.borderWidth = 2.0
+            sender.layer.borderColor = warnColor.cgColor
+            print("Text fields all have data ... ")
+//            let headers = [ "Content-Type":"application/xml", ]
+//            // Custom Body Encoding
+//            struct RawDataEncoding: ParameterEncoding {
+//                public static var `default`: RawDataEncoding { return RawDataEncoding() }
+//                public func encode(_ urlRequest: URLRequestConvertible, with parameters: Parameters?) throws -> URLRequest {
+//                    var request = try urlRequest.asURLRequest()
+//                    request.httpBody = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><mobile_device_command><general><command>EnableLostMode</command><lost_mode_message>\(LostMode.lostModeSettings.lostModeMessage!)</lost_mode_message><lost_mode_phone>\(LostMode.lostModeSettings.lostModeNumber!)</lost_mode_phone><lost_mode_footnote>\(LostMode.lostModeSettings.lostModeFootNote!)</lost_mode_footnote><always_enforce_lost_mode>\(LostMode.lostModeSettings.lostModeForced!)</always_enforce_lost_mode><lost_mode_with_sound>\(LostMode.lostModeSettings.lostModeSound!)</lost_mode_with_sound></general><mobile_devices><mobile_device><id>\(workingData.deviceID)</id></mobile_device></mobile_devices></mobile_device_command>".data(using: String.Encoding.utf8, allowLossyConversion: false)
+//                    return request
+//                }
+//            }
+//
+//            // Fetch Request
+//            Alamofire.request(workingjss.jssURL + devLostModePath, method: .post, encoding: RawDataEncoding.default, headers: headers)
+//                .authenticate(user: workingjss.jssUsername, password: workingjss.jssPassword)
+//                .validate(statusCode: 200..<300)
+//                .responseString { LMresponse in
+//                    if (LMresponse.result.error == nil) { sender.layer.borderColor = successColor.cgColor }
+//                    else { sender.layer.borderColor = failColor.cgColor }
+//            } // Close Alamofire Request
+        }
+        else {
+            print("One or more fields are empty")
+            sender.layer.borderWidth = 1.0
+            sender.layer.borderColor = UIColor.lightGray.cgColor
+            if !lostModeMessageText.hasText {
+                lostModeMessageText.layer.borderColor = failColor.cgColor
+                lostModeMessageText.layer.borderWidth = 2.0
             }
+            else { defaultsLM.set(lostModeMessageText.text, forKey: "savedLostModeMessage") }
+            if !lostModePhoneNumberText.hasText {
+                lostModePhoneNumberText.layer.borderColor = failColor.cgColor
+                lostModePhoneNumberText.layer.borderWidth = 2.0
+            }
+            else { LostMode.lostModeSettings.lostModeNumber = lostModePhoneNumberText.text }
+            if !lostModeFootnoteText.hasText {
+                lostModeFootnoteText.layer.borderColor = failColor.cgColor
+                lostModeFootnoteText.layer.borderWidth = 2.0
+            }
+            else { LostMode.lostModeSettings.lostModeFootNote = lostModeFootnoteText.text }
         }
-        
-        // Fetch Request
-        //print(workingjss.jssURL + devLostModePath)
-        Alamofire.request(workingjss.jssURL + devLostModePath, method: .post, encoding: RawDataEncoding.default, headers: headers)
-            .authenticate(user: workingjss.jssUsername, password: workingjss.jssPassword)
-            .validate(statusCode: 200..<300)
-            .responseString { LMresponse in
-                if (LMresponse.result.error == nil) {
-                    print("Successful Command")
-                    sender.layer.borderColor = successColor.cgColor
-                    //debugPrint("HTTP Response Body: \(response.data)")
-                }
-                else {
-                    print("Failed Command")
-                    sender.layer.borderColor = failColor.cgColor
-                    //debugPrint("HTTP Request failed: \(response.result.error)")
-                }
-        }
-        
+    } // Close enableLostModePresed Function
+    
+    func setButtonColors(color: CGColor, width: Float) {
+        lostModeMessageText.layer.borderColor = color
+        lostModeMessageText.layer.borderWidth = CGFloat(width)
+        lostModePhoneNumberText.layer.borderColor = color
+        lostModePhoneNumberText.layer.borderWidth = CGFloat(width)
+        lostModeFootnoteText.layer.borderColor = color
+        lostModeFootnoteText.layer.borderWidth = CGFloat(width)
     }
+    
 }
